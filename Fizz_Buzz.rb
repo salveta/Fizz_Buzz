52534c61d15811583f8aff728199633d81ab2daa
salveta <salveta4_@hotmail.com>
2014-07-02 12:12:48 +0200

    Kata end

--- Fizz_Buzz.rb
+++ Fizz_Buzz.rb
@@ -1,6 +1,24 @@
 class Fizz_Buzz
 
-def self.play(number)
-end
+	FIZZ="Fizz"
+	BUZZ="Buzz"
+	FIZZBUZZ="FizzBuzz"
 
+	def self.play(number)
+		result=""	
+		result+=say_fizz(number)
+		result+= say_buzz(number)
+		result=number if result==""
+		result
+	end
+
+	def self.say_fizz(number)
+		return FIZZ if number %3 == 0
+		""
+	end
+
+	def self.say_buzz(number)
+		return BUZZ if number %5 == 0
+		""
+	end
 end
