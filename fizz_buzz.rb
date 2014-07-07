b93f6d9e1f93267b2ad8d04ed1b7937b273b4235
salveta <salveta4_@hotmail.com>
2014-07-07 13:52:07 +0200

    End kata refactor

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -1,5 +1,40 @@
 class Fizz_Buzz
- 
 
+FIZZBUZZ="FizzBuzz"
+FIZZ="Fizz"
+BUZZ="Buzz"
+FIZZNUMBER= % 3 == 0
+BUZZNUMBER= % 5 == 0
 
+	def self.fizzbuzz (number)
+		result=""
+		result+= say_fizz(number)
+		result+= say_buzz(number)
+		result= number if nor_fizz_nor_buzz?(number)
+		result
+	end
+
+private
+
+	def self.say_fizz(number)
+		return FIZZ if multiple_of_three?(number)
+		""
+	end
+
+	def self.say_buzz(number)
+		return BUZZ if multiple_of_five?(number)
+		""
+	end
+
+	def self.multiple_of_three?(number)
+		number % 3 == 0
+	end
+
+	def self.multiple_of_five?(number)
+		number % 5 == 0
+	end
+
+	def self.nor_fizz_nor_buzz?(number)
+	(!multiple_of_three?(number) && (!multiple_of_five?(number)))
+	end
 end
--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -1,7 +1,24 @@
 require './fizz_buzz'
 
-describe "Fizz_Buzz" do
-	
+describe "FizzBuzz" do
+  it "replies the same number" do
+    expect(Fizz_Buzz.fizzbuzz(1)).to eq(1)
+    expect(Fizz_Buzz.fizzbuzz(2)).to eq(2)
+  end
 
+   it "replies fizz" do
+    expect(Fizz_Buzz.fizzbuzz(3)).to eq('Fizz')
+    expect(Fizz_Buzz.fizzbuzz(6)).to eq('Fizz')
+    expect(Fizz_Buzz.fizzbuzz(9)).to eq('Fizz')
+  end
+
+   it "replies buzz" do
+    expect(Fizz_Buzz.fizzbuzz(5)).to eq('Buzz')
+    expect(Fizz_Buzz.fizzbuzz(10)).to eq('Buzz')
+  end
+
+   it "replies fizzbuzz" do
+    expect(Fizz_Buzz.fizzbuzz(15)).to eq('FizzBuzz')
+  end
    
 end
